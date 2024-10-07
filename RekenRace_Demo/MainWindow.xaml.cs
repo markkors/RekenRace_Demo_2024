e1d12636c0d522df7f383c897cf4a0dfee6a090c
markk <m.kors@rocmn.nl>
2024-10-07 09:27:51 +0200

    Add project files.

---
+++ RekenRace_Demo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RekenRace_Demo.Models;
+
+namespace RekenRace_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<oSom> sommen = new List<oSom>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create a new instance of the oSom class
+            
+            Random rnd = new Random();
+
+            // create 10 sums
+            for (int i = 0; i < 10; i++)
+            {
+                oSom som = new oSom();
+                // Generate a random number between 1 and 10
+
+                som.Part1 = rnd.Next(1, 10);
+                som.Part2 = rnd.Next(1, 10);
+
+                // Generate a random operator
+                string[] operators = { "+", "-", "*", "/" };
+                som.Operator = operators[rnd.Next(0, 4)];
+
+                // add the sum to the collection
+                sommen.Add(som);
+            }
+            
+
+        }
+    }
+}
---
+++ RekenRace_Demo/Models/oSom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RekenRace_Demo.Models
+{
+    internal class oSom
+    {
+        public int Part1 { get; set; }
+        public int Part2 { get; set; }  
+        public string Operator { get; set; }
+
+        public string Solution { 
+            get {
+                float result = 0;
+                switch (Operator)
+                {
+                    case "*":
+                        result = Part1 * Part2;
+                        break;
+                    case "-":
+                        result = Part1 - Part2;
+                        break;
+                    case "+":
+                        result = Part1 + Part2;
+                        break;
+                    case "/":
+                        result = Part1 / Part2;
+                        break;
+
+                }
+                return result.ToString();
+
+            }
+
+        }
+
+
+    }
+}
